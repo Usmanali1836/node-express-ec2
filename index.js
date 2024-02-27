65be822711acfe58a384b350f5fb5d500b302daa
UsmanAli <ali2051448@gmail.com>
2024-02-28 02:14:33 +0500

    first commit

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.listen(5002, () => console.log("Api running on port 5002"));
 
-app.get("/", (req, res) => res.json("My Api running"));
+app.get("/", (req, res) => res.json("My Api run"));
